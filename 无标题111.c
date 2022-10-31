4652a0b321d439ce86b67d08a8017c380b67935b
Mochizuki Shigure <84255253+MochizukiShigure@users.noreply.github.com>
2022-10-31 19:32:20 +0800

    Update 无标题111.c

--- 无标题111.c
+++ 无标题111.c
@@ -1,11 +1,11 @@
-/* Exercise 1 (20%)
+/* Exercise 1
 Write a C program that can perform the following:
- Read a string from the keyboard with at least 2 characters which are digits (0-9);
- Extract all digits (0-9) from the string and create a new string containing only those digits;
- Determine the length of the digit string, and print the length of the string on the screen.
- Print the digit string on the screen in reverse (DO NOT reverse the characters in the array
+* Read a string from the keyboard with at least 2 characters which are digits (0-9);
+* Extract all digits (0-9) from the string and create a new string containing only those digits;
+* Determine the length of the digit string, and print the length of the string on the screen.
+* Print the digit string on the screen in reverse (DO NOT reverse the characters in the array
 or use the reverse function).
- Print the integer value of the reversed digit string.
+* Print the integer value of the reversed digit string.
 Example
 input is: CPT109 has 10 lectures.
 output is:
